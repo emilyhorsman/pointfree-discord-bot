f2b5b5d164d2ee464465f825c1f9a1bd995fc79b
Emily Horsman <me@emilyhorsman.com>
2019-02-03 09:41:42 -0500

    Begin some cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -4,44 +4,48 @@ module Main where
 
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Exception (finally)
+import Data.Bool (bool)
 import Data.Monoid ((<>))
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import Lib
-import Discord
+import qualified Discord as D
 import Pointfree
 
-pointfreeApp :: IO ()
-pointfreeApp = do
-  token <- T.strip <$> TIO.readFile "./auth-token.secret"
-  discord <- loginRestGateway (Auth token)
-  finally (app discord) (stopDiscord discord)
+entryPointApp :: IO ()
+entryPointApp = do
+  discord <- loginWithToken
+  finally (app discord) (D.stopDiscord discord)
 
-app :: (RestChan, Gateway, z) -> IO ()
-app discord = do
-  e <- nextEvent discord
-  case e of
-    Left err ->
-      print err
-
-    Right (MessageCreate message) -> do
-      print message
-      respond discord message
+app :: Server -> IO ()
+app discord =
+  D.nextEvent discord >>= either print handleEvent
+  where
+    handleEvent (D.MessageCreate message) =
+      handleMessageCreate discord message >> app discord
+    handleEvent _ =
       app discord
 
-    _ ->
-      app discord
+handleMessageCreate :: Server -> D.Message -> IO ()
+handleMessageCreate discord message =
+  either return (respond discord) $ (parseMessageType <$> (isFromBot message >>= isPrompt))
 
-respond discord message
-  | fromBot message = return ()
-  | isPrompt message = handle discord message
-  | otherwise = return ()
+data MessageType
+  = Help
+  | BangPointfree T.Text
+  deriving Show
 
-isPrompt :: Message -> Bool
-isPrompt Message { messageText = t } =
-  T.isPrefixOf "!pf " t
+parseMessageType :: D.Message -> MessageType
+parseMessageType _ = Help
 
-handle discord message@(Message { messageText = t }) =
+respond :: Server -> MessageType -> IO ()
+respond discord _ = return ()
+
+isPrompt :: D.Message -> Either () D.Message
+isPrompt message@(D.Message { D.messageText = t }) =
+  bool (Left ()) (Right message) $ T.isPrefixOf "!pf " t
+
+handle discord message@(D.Message { D.messageText = t }) =
   let
     expr = T.strip <$> (T.stripPrefix "!pf " t >>= T.stripPrefix "`" >>= T.stripSuffix "`")
 
@@ -50,12 +54,7 @@ handle discord message@(Message { messageText = t }) =
     case result of
       Nothing -> return ()
       Just msg ->
-        restCall discord (CreateMessage (messageChannel message) (T.pack ("`" ++ msg ++ "`"))) >> return ()
-
-fromBot Message { messageAuthor = author } =
-  case author of
-    Webhook -> True
-    User { userIsBot = isBot } -> isBot
+        D.restCall discord (D.CreateMessage (D.messageChannel message) (T.pack ("`" ++ msg ++ "`"))) >> return ()
 
 main :: IO ()
-main = pointfreeApp
+main = entryPointApp
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
 module Lib
-    ( someFunc
+    ( Server
+    , isFromBot
+    , loginWithToken
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Bool (bool)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import qualified Discord as D
+
+
+type Server = (D.RestChan, D.Gateway, [D.ThreadIdType])
+
+
+loginWithToken :: IO Server
+loginWithToken =
+  T.strip <$> TIO.readFile "./auth-token.secret" >>= D.loginRestGateway . D.Auth
+
+
+isFromBot :: D.Message -> Either () D.Message
+isFromBot message@(D.Message { D.messageAuthor = author }) =
+  case author of
+    D.Webhook -> Left ()
+    D.User { D.userIsBot = isBot } ->
+      bool (Left ()) (Right message) isBot
